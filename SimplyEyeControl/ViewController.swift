ef3a18f92d4b5b9cea4db9644a70190cad0fe655
Jijun Lu <jijunlu@gmail.com>
2018-06-09 22:30:22 -0400

    Initial Commit

---
+++ SimplyEyeControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplyEyeControl
+//
+//  Created by Jijun Lu on 6/9/18.
+//  Copyright © 2018 Jijun Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
